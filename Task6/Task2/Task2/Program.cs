8b41dc6b6bb57acbcd5eddb3cc2d1a06e5d2a426
Aida <aida.zaynullina1@gmail.com>
2018-07-23 14:47:29 +0400

    Task6

---
+++ Task6/Task2/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public delegate void Greeting(Person person, TimeSpan time);
+
+    public delegate void Farewell(Person person);
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Office office = new Office();
+            Person john = new Person("John");
+            Person bill = new Person("Bill");
+            Person hugo = new Person("Hugo");
+
+            office.AppearenceOfAPerson(john, new TimeSpan(10, 00, 00));
+            office.AppearenceOfAPerson(bill, new TimeSpan(12, 00, 00));
+            office.AppearenceOfAPerson(hugo, new TimeSpan(17, 00, 00));
+
+            office.OutAPerson(john);
+            office.OutAPerson(bill);
+            office.OutAPerson(hugo);
+
+            Console.ReadLine();
+        }
+    }
+}
